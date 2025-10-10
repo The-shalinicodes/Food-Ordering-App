3eceaadd717aa41923e90878415900a812ff1f73
Shalu <shlokashalini@gmail.com>
2025-10-10 14:18:28 +0530

    Added Restaurant card

--- app.js
+++ app.js
@@ -23,10 +23,36 @@ const Header = () => {
   );
 };
 
+const RestaurantCard=()=>{
+  return(
+    <div className="res-card" style={{backgroundColor:"#f0f0f0"}}>
+      <img 
+      className="res-logo"
+      alt="res-logo" 
+      src="https://www.shutterstock.com/image-photo/traditional-chicken-biryani-served-brass-600nw-2622739739.jpg"></img>
+      <h3>Meghana Foods</h3>
+      <h4>Biryani, Noth Indian,Asian</h4>
+      <h4>4.4 stars</h4>
+      <h4>38 minutes</h4>
+    </div>
+  );
+}
+
+const Body=()=>{
+  return(
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-container"></div>
+      <RestaurantCard/>
+    </div>
+  );
+}
+
 const AppLayout = () => {
   return (
     <div className="app">
       <Header />
+      <Body/>
     </div>
   ); 
 };
