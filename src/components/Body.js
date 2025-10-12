143c83888a2f8eb18e42e7e75dfa582b208775ce
Shalu <shlokashalini@gmail.com>
2025-10-12 10:01:26 +0530

    Added Top Rated Button with onClick Functionality

--- src/components/Body.js
+++ src/components/Body.js
@@ -5,7 +5,13 @@ const Body=()=>{
   return(
     <div className="body">
       <div className="filter">
-        <button className="filter-btn">Top Rated Restaurants</button>
+        <button 
+          className="filter-btn" 
+          onClick={()=>{
+            console.log("Button Clicked")}}
+            >
+              Top Rated Restaurants
+            </button>
       </div>
       <div className="res-container">
         <RestaurantCard
