cb35a94d5fe800cfa4d056cd25f57712608c7bc5
Shalu <shlokashalini@gmail.com>
2025-10-18 11:22:21 +0530

    Understing the concept of Live data fetch

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,15 +1,26 @@
 import React from "react";
 import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/data";
-import {useState} from "react";
+import {useState,useEffect} from "react";
 
 const Body=()=>{
   //Local State variable-SuperPowerful Variable
   const arr=useState(resList);
-  // const[listOfRestaurants,setListOfRestaurant]=arr;//array destructuring
+  const[listOfRestaurants,setListOfRestaurant]=arr;//array destructuring
+  // const listOfRestaurants=arr[0];
+  // const setListOfRestaurant=arr[1];
+  useEffect(()=>{
+    console.log("useEffect Called");
+    fetchData();
+  },[]);
 
-  const listOfRestaurants=arr[0];
-  const setListOfRestaurant=arr[1];
+  const fetchData=async()=>{
+    const data=await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.97&lng=77.59");
+    const json=await data.json();
+    console.log(json);
+    
+  }
+
   return(
     <div className="body">
       <div className="filter">
