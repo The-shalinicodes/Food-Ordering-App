393afec4d0a36a3f23ad7c7e72353368dd24ad72
Shalu <shlokashalini@gmail.com>
2025-12-05 22:46:57 +0530

    Worked with live Swiggy API

---
+++ App.js
@@ -0,0 +1,649 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img 
+          className="logo"
+          src="https://m.media-amazon.com/images/I/41UaA6ncmbL.png"
+        />
+      </div>
+
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const RestaurantCard = ({ resData }) => {
+  // Normalizing structure
+  const info = resData?.card?.card?.info || resData?.info;
+
+  if (!info) return null; // Safety check
+
+  const {
+    cloudinaryImageId,
+    name,
+    cuisines,
+    avgRating,
+    costForTwo,
+    sla,
+    deliveryTime,
+  } = info;
+
+  return (
+    <div className="res-card">
+      <img
+        className="res-logo"
+        alt="res-logo"
+        src={
+          "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" +
+          cloudinaryImageId
+        }
+      />
+
+      <h3>{name}</h3>
+      <h4>{cuisines?.join(", ")}</h4>
+      <h4>{avgRating} stars</h4>
+      <h4>{costForTwo}</h4>
+      <h4>{sla?.deliveryTime || deliveryTime} minutes</h4>
+    </div>
+  );
+};
+
+
+
+const resList = [
+  {
+    "card": {
+      "card": {
+        "@type": "type.googleapis.com/swiggy.gandalf.widgets.v2.CollectionMasthead",
+        "collectionId": "83639",
+        "title": "Biryani",
+        "description": "Taste these delectable classics, delectable biryanis to make your day.",
+        "imageId": "COLLECTIONS/IMAGES/MERCH/2025/3/7/6b765643-8f10-4176-b1f4-5e096d4182c0_biryani nv (3) (1).png",
+        "aspectRatio": "3.44",
+        "cta": {
+          "link": "swiggy://collectionV2?collection_id=83639&tags=layout_CCS_Biryani,biryani,ads_pc_biryani",
+          "type": "collectionv2"
+        },
+        "type": "COLLECTION_MASTHEAD_TYPE_IMAGE_WITH_TEXT",
+        "count": "49 restaurants",
+        "navBarConfig": {}
+      }
+    }
+  },
+  {
+    "card": {
+      "card": {
+        "@type": "type.googleapis.com/swiggy.gandalf.widgets.v2.InlineViewFilterSortWidget",
+        "sortConfigs": [
+          {
+            "key": "relevance",
+            "title": "Relevance (Default)",
+            "selected": true,
+            "defaultSelection": true
+          },
+          {
+            "key": "deliveryTimeAsc",
+            "title": "Delivery Time"
+          },
+          {
+            "key": "modelBasedRatingDesc",
+            "title": "Rating"
+          },
+          {
+            "key": "costForTwoAsc",
+            "title": "Cost: Low to High"
+          },
+          {
+            "key": "costForTwoDesc",
+            "title": "Cost: High to Low"
+          }
+        ],
+        "restaurantCount": 49,
+        "facetList": [
+          {
+            "label": "10 Mins Delivery",
+            "id": "isRestaurantBolt",
+            "selection": "SELECT_TYPE_SINGLESELECT",
+            "facetInfo": [
+              {
+                "label": "10 Mins Delivery",
+                "id": "isRestaurantBoltfacetquery0",
+                "analytics": {},
+                "openFilter": true
+              }
+            ],
+            "viewType": "VIEW_TYPE_FLATTENED",
+            "subLabel": "Filterby",
+            "icon": "COLLECTIONS/IMAGES/MERCH/2024/12/18/4b2e8903-0e25-401b-8ede-088491b4cfa0_937977a3-b03b-4a9e-8b6a-24937664d1a9_pic.png",
+            "selectedIcon": "COLLECTIONS/IMAGES/MERCH/2024/12/26/3072d307-2f8e-471c-862d-d426fb93c0bf_4b2e8903-0e25-401b-8ede-088491b4cfa0_937977a3-b03b-4a9e-8b6a-24937664d1a9_pic.png"
+          }
+        ],
+        "widgetId": "inlineFacetFilter"
+      }
+    }
+  },
+  {
+  "card": {
+    "card": {
+      "@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+      "info": {
+        "id": "221188",
+        "name": "The Biryani Life",
+        "cloudinaryImageId": "pfqy0xejorylde2a3jjb",
+        "locality": "Abbanna Colony",
+        "areaName": "Tata Nagar",
+        "costForTwo": "₹250 for two",
+        "cuisines": [
+          "Biryani",
+          "Mughlai",
+          "Lucknowi",
+          "Hyderabadi",
+          "Kebabs",
+          "Desserts",
+          "Beverages"
+        ],
+        "avgRating": 3.8,
+        "parentId": "8496",
+        "avgRatingString": "3.8",
+        "totalRatingsString": "1.4K+",
+        "promoted": true,
+        "adTrackingId": "cid=c273e83f-b26d-4c48-8ee8-0f34ba713c9c~p=0~adgrpid=c273e83f-b26d-4c48-8ee8-0f34ba713c9c#ag52~mp=SWIGGY_IN~bl=FOOD~aet=RESTAURANT~aeid=221188~plpr=COLLECTION~eid=6abd8c99-7c3c-4148-bac4-a350f7805f74~srvts=1764912242946~collid=83639",
+        "sla": {
+          "deliveryTime": 24,
+          "lastMileTravel": 2.3,
+          "serviceability": "SERVICEABLE",
+          "slaString": "20-25 mins",
+          "lastMileTravelString": "2.3 km",
+          "iconType": "ICON_TYPE_EMPTY"
+        },
+        "availability": {
+          "nextCloseTime": "2025-12-05 23:59:00",
+          "opened": true
+        },
+        "badges": {
+          "imageBadges": [
+            {
+              "imageId": "android/static-assets/icons/big_rx.png",
+              "description": "bolt!"
+            }
+          ]
+        },
+        "isOpen": true,
+        "type": "F",
+        "badgesV2": {
+          "entityBadges": {
+            "textExtendedBadges": {},
+            "textBased": {},
+            "imageBased": {
+              "badgeObject": [
+                {
+                  "attributes": {
+                    "description": "bolt!",
+                    "imageId": "android/static-assets/icons/big_rx.png"
+                  }
+                }
+              ]
+            }
+          }
+        }
+      },
+      "aggregatedDiscountInfoV3": {
+        "header": "ITEMS",
+        "subHeader": "AT ₹179",
+        "logoCtx": {
+          "text": "BENEFITS"
+        }
+      },
+      "orderabilityCommunication": {
+        "title": {},
+        "subTitle": {},
+        "message": {},
+        "customIcon": {},
+        "commsStyling": {}
+      },
+      "differentiatedUi": {
+        "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        "differentiatedUiMediaDetails": {
+          "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+          "lottie": {},
+          "video": {}
+        }
+      },
+      "reviewsSummary": {},
+      "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      "restaurantOfferPresentationInfo": {},
+      "externalRatings": {
+        "aggregatedRating": {
+          "rating": "--"
+        }
+      },
+      "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+      "campaignId": "c273e83f-b26d-4c48-8ee8-0f34ba713c9c"
+    },
+    "analytics": {},
+    "cta": {
+      "link": "swiggy://menu?restaurant_id=221188&source=collection&query=Biryani",
+      "text": "RESTAURANT_MENU",
+      "type": "DEEPLINK"
+    },
+    "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food"
+  },
+  "relevance": {
+    "type": "RELEVANCE_TYPE_ON_MENU_RETURN",
+    "sectionId": "MENU_RETURN_FOOD"
+  }
+  },
+  {
+  "card": {
+    "card": {
+      "@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+      "info": {
+        "id": "830262",
+        "name": "UBQ by Barbeque Nation",
+        "cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2025/7/10/5cfc9478-c7bb-40d9-89fe-fd4339b5af3e_128159.JPG",
+        "locality": "Mathru Layout",
+        "areaName": "Thanisandra",
+        "costForTwo": "₹300 for two",
+        "cuisines": [
+          "North Indian",
+          "Barbecue",
+          "Biryani",
+          "Kebabs",
+          "Desserts"
+        ],
+        "avgRating": 4.1,
+        "parentId": "10804",
+        "avgRatingString": "4.1",
+        "totalRatingsString": "147",
+        "sla": {
+          "deliveryTime": 34,
+          "lastMileTravel": 6.2,
+          "serviceability": "SERVICEABLE",
+          "slaString": "30-35 mins",
+          "lastMileTravelString": "6.2 km",
+          "iconType": "ICON_TYPE_EMPTY"
+        },
+        "availability": {
+          "nextCloseTime": "2025-12-06 00:00:00",
+          "opened": true
+        },
+        "badges": {},
+        "isOpen": true,
+        "type": "F",
+        "badgesV2": {
+          "entityBadges": {
+            "textExtendedBadges": {},
+            "textBased": {},
+            "imageBased": {}
+          }
+        },
+        "aggregatedDiscountInfoV3": {
+          "header": "50% OFF",
+          "subHeader": "UPTO ₹100"
+        }
+      },
+      "orderabilityCommunication": {
+        "title": {},
+        "subTitle": {},
+        "message": {},
+        "customIcon": {},
+        "commsStyling": {}
+      },
+      "differentiatedUi": {
+        "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        "differentiatedUiMediaDetails": {
+          "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+          "lottie": {},
+          "video": {}
+        }
+      },
+      "reviewsSummary": {},
+      "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      "restaurantOfferPresentationInfo": {},
+      "externalRatings": {
+        "aggregatedRating": {
+          "rating": "--"
+        }
+      },
+      "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+    },
+    "analytics": {},
+    "cta": {
+      "link": "swiggy://menu?restaurant_id=830262&source=collection&query=Biryani",
+      "text": "RESTAURANT_MENU",
+      "type": "DEEPLINK"
+    },
+    "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food"
+  },
+  "relevance": {
+    "type": "RELEVANCE_TYPE_ON_MENU_RETURN",
+    "sectionId": "MENU_RETURN_FOOD"
+  }
+  },
+  {
+  "card": {
+    "card": {
+      "@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+      "info": {
+        "id": "426807",
+        "name": "ITC Master Chef Creations",
+        "cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2025/7/10/5cfc9478-c7bb-40d9-89fe-fd4339b5af3e_128159.JPG",
+        "locality": "Nagawara",
+        "areaName": "Manyata Tech Park",
+        "costForTwo": "₹650 for two",
+        "cuisines": [
+          "North Indian",
+          "Biryani",
+          "Kebabs",
+          "Mediterranean",
+          "Mughlai"
+        ],
+        "avgRating": 4.6,
+        "parentId": "11992",
+        "avgRatingString": "4.6",
+        "totalRatingsString": "942",
+        "sla": {
+          "deliveryTime": 34,
+          "lastMileTravel": 4.7,
+          "serviceability": "SERVICEABLE",
+          "slaString": "30-35 mins",
+          "lastMileTravelString": "4.7 km",
+          "iconType": "ICON_TYPE_EMPTY"
+        },
+        "availability": {
+          "nextCloseTime": "2025-12-05 23:59:00",
+          "opened": true
+        },
+        "badges": {},
+        "isOpen": true,
+        "type": "F",
+        "badgesV2": {
+          "entityBadges": {
+            "textExtendedBadges": {},
+            "textBased": {},
+            "imageBased": {}
+          }
+        },
+        "aggregatedDiscountInfoV3": {
+          "header": "25% OFF",
+          "subHeader": "UPTO ₹125"
+        }
+      },
+      "orderabilityCommunication": {
+        "title": {},
+        "subTitle": {},
+        "message": {},
+        "customIcon": {},
+        "commsStyling": {}
+      },
+      "differentiatedUi": {
+        "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        "differentiatedUiMediaDetails": {
+          "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+          "lottie": {},
+          "video": {}
+        }
+      },
+      "reviewsSummary": {},
+      "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      "restaurantOfferPresentationInfo": {},
+      "externalRatings": {
+        "aggregatedRating": {
+          "rating": "--"
+        }
+      },
+      "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+    },
+    "analytics": {},
+    "cta": {
+      "link": "swiggy://menu?restaurant_id=426807&source=collection&query=Biryani",
+      "text": "RESTAURANT_MENU",
+      "type": "DEEPLINK"
+    },
+    "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food"
+  },
+  "relevance": {
+    "type": "RELEVANCE_TYPE_ON_MENU_RETURN",
+    "sectionId": "MENU_RETURN_FOOD"
+  }
+  },
+  {
+  "card": {
+    "card": {
+      "@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+      "info": {
+        "id": "330275",
+        "name": "Meghana Foods",
+        "cloudinaryImageId": "e0vvulfbahjxjz6k4uwi",
+        "locality": "Nagawara",
+        "areaName": "Nagawara",
+        "costForTwo": "₹500 for two",
+        "cuisines": [
+          "Biryani",
+          "Andhra",
+          "South Indian",
+          "Chinese",
+          "Seafood"
+        ],
+        "avgRating": 4.5,
+        "parentId": "635",
+        "avgRatingString": "4.5",
+        "totalRatingsString": "21K+",
+        "promoted": false,
+        "sla": {
+          "deliveryTime": 28,
+          "lastMileTravel": 3.9,
+          "serviceability": "SERVICEABLE",
+          "slaString": "25-30 mins",
+          "lastMileTravelString": "3.9 km",
+          "iconType": "ICON_TYPE_EMPTY"
+        },
+        "availability": {
+          "nextCloseTime": "2025-12-06 01:00:00",
+          "opened": true
+        },
+        "badges": {
+          "imageBadges": [
+            {
+              "imageId": "newg.png",
+              "description": "Gourmet"
+            }
+          ]
+        },
+        "isOpen": true,
+        "type": "F",
+        "badgesV2": {
+          "entityBadges": {
+            "textExtendedBadges": {},
+            "textBased": {},
+            "imageBased": {
+              "badgeObject": [
+                {
+                  "attributes": {
+                    "imageId": "newg.png",
+                    "description": "Gourmet"
+                  }
+                }
+              ]
+            }
+          }
+        },
+        "aggregatedDiscountInfoV3": {
+          "header": "10% OFF",
+          "subHeader": "UPTO ₹40"
+        }
+      },
+      "differentiatedUi": {
+        "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        "differentiatedUiMediaDetails": {
+          "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+          "lottie": {},
+          "video": {}
+        }
+      },
+      "reviewsSummary": {},
+      "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      "restaurantOfferPresentationInfo": {},
+      "externalRatings": {
+        "aggregatedRating": {
+          "rating": "4.3",
+          "ratingCount": "5K+"
+        }
+      },
+      "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+    },
+    "analytics": {},
+    "cta": {
+      "link": "swiggy://menu?restaurant_id=330275&source=collection&query=Biryani",
+      "text": "RESTAURANT_MENU",
+      "type": "DEEPLINK"
+    },
+    "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food"
+  },
+  "relevance": {
+    "type": "RELEVANCE_TYPE_ON_MENU_RETURN",
+    "sectionId": "MENU_RETURN_FOOD"
+  }
+  },
+  {
+  info: {
+    id: "128566",
+    name: "NIC Ice Creams",
+    cloudinaryImageId: "FOOD_CATALOG/IMAGES/CMS/2024/4/15/79916c92-2cc5-4468-9bb5-8ff18534aca3_2f8cef4a-e009-4556-a959-097da0f76a3d.jpg",
+    cuisines: ["Ice Cream", "Desserts"],
+    avgRating: 4.6,
+    costForTwo: 150,
+    deliveryTime: 20
+  }
+  },
+  {
+  info: {
+    id: "254123",
+    name: "Pizza Hut",
+    cloudinaryImageId: "RX_THUMBNAIL/IMAGES/VENDOR/2025/9/1/029648eb-d240-40b8-8ca8-55b71491db4c_408184.JPG",
+    cuisines: ["Pizzas"],
+    avgRating: 4.1,
+    costForTwo: 350,
+    deliveryTime: 35
+  }
+  },
+  {
+  info: {
+    id: "652987",
+    name: "Chinese Wok",
+    cloudinaryImageId: "v1677221341/29bd9b1680ebe21346fae76dc763a1a2.jpg",
+    cuisines: ["Chinese", "Asian"],
+    avgRating: 4.3,
+    costForTwo: 250,
+    deliveryTime: 30
+  }
+  },
+  {
+  info: {
+    id: "785421",
+    name: "Shah Ghouse Hotel",
+    cloudinaryImageId: "FOOD_CATALOG/IMAGES/CMS/2025/9/17/533a96b1-d853-4cce-adc3-df9f5e5cb822_bdbac4e3-c34d-42e2-a97c-0970b141332f.jpg",
+    cuisines: ["Biryani", "Mughlai"],
+    avgRating: 4.4,
+    costForTwo: 400,
+    deliveryTime: 32
+  }
+  },
+  {
+  info: {
+    id: "456932",
+    name: "Chai Point",
+    cloudinaryImageId: "RX_THUMBNAIL/IMAGES/VENDOR/2025/8/17/894cc6f3-58df-45f9-8ae5-f6362bddd2a6_1173378.jpg",
+    cuisines: ["Tea", "Snacks", "Beverages"],
+    avgRating: 4.2,
+    costForTwo: 150,
+    deliveryTime: 18
+  }
+  },
+  {
+  info: {
+    id: "389755",
+    name: "New Paradise Biryani",
+    cloudinaryImageId: "0b3f8d29fca58178e71a3564fb681195",
+    cuisines: ["Biryani", "North Indian"],
+    avgRating: 4.3,
+    costForTwo: 500,
+    deliveryTime: 40
+  }
+  },
+  {
+  info: {
+    id: "998877",
+    name: "Baskin Robbins",
+    cloudinaryImageId: "RX_THUMBNAIL/IMAGES/VENDOR/2025/4/23/995874ea-a7e1-448e-bd08-c2e5c6ca62e9_858476.jpg",
+    cuisines: ["Ice Cream", "Desserts"],
+    avgRating: 4.5,
+    costForTwo: 200,
+    deliveryTime: 25
+  }
+  },
+  {
+  info: {
+    id: "112233",
+    name: "Burger King",
+    cloudinaryImageId: "RX_THUMBNAIL/IMAGES/VENDOR/2025/6/18/a380c62f-51c3-4ba8-a616-bcd45eb93eb3_808105.jpg",
+    cuisines: ["Burgers", "Fast Food"],
+    avgRating: 4.0,
+    costForTwo: 300,
+    deliveryTime: 30
+  }
+  },
+  {
+  info: {
+    id: "556677",
+    name: "Domino's Pizza",
+    cloudinaryImageId: "RX_THUMBNAIL/IMAGES/VENDOR/2025/11/11/c0e21b7e-60b1-4fae-8ac4-17bfe0241e4d_23842.JPG",
+    cuisines: ["Pizza", "Fast Food"],
+    avgRating: 4.1,
+    costForTwo: 350,
+    deliveryTime: 35
+  }
+  },
+];
+
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="Search">Search</div>
+
+      <div className="res-container">
+        {resList.map((restaurant) => (
+          <RestaurantCard
+            key={restaurant?.info?.id || restaurant?.card?.card?.info?.id || Math.random()}
+            resData={restaurant}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout />);
--- src/utils/constants.js
+++
@@ -1,6 +0,0 @@
-export const LOGO_URL=
-    "https://img.freepik.com/free-vector/food-shopping-logo-template-design_460848-10299.jpg";
-
-export const BIRYANI_URL=
-    "https://www.shutterstock.com/image-photo/traditional-chicken-biryani-served-brass-600nw-2622739739.jpg";
-
