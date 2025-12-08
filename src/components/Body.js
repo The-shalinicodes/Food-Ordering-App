4f0116167ca5e5264858458187bf444b86ebb913
Shalu <shlokashalini@gmail.com>
2025-12-08 10:54:25 +0530

    Applied the UseState Variable on resList to filter the top rated restaurants

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,14 +1,31 @@
 import RestaurantCard from "./RestaurantCard.js";
+import {useState} from "react";//should be imported as named import
 import resList from "./utils/mockData.js";
 
-
 const Body = () => {
+  //State Variable- Super powerful variable
+  const [listOfRestaurants,setListOfRestaurant]=useState(resList);
+
+
   return (
     <div className="body">
-      <div className="Search">Search</div>
+      <div className="filter">
+        <button className="filter-btn" 
+        onClick={()=>{
+          
+          const filteredList = listOfRestaurants.filter((res) => {
+            const info = res?.card?.card?.info || res?.info;
+            return info?.avgRating >= 4.0;
+          });
+          setListOfRestaurant(filteredList);
+        }}
+      >
+        Top Rated Restaurants
+      </button>
+      </div>
 
       <div className="res-container">
-        {resList.map((restaurant) => (
+        {listOfRestaurants.map((restaurant) => (
           <RestaurantCard
             key={restaurant?.info?.id || restaurant?.card?.card?.info?.id || Math.random()}
             resData={restaurant}
